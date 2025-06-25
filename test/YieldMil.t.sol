9c870211e4423c1bcfc8091419526767640bb674
nzmpi <nizami.abdulov@gmail.com>
2025-06-25 16:32:35 +0400

    test: remove mocks

--- test/YieldMil.t.sol
+++ test/YieldMil.t.sol
@@ -1,42 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {ProxyBase} from "../src/ProxyBase.sol";
-import {YieldMil} from "../src/YieldMil.sol";
-
-import "../src/interfaces/ISystemContract.sol";
-import {MockERC20} from "./helpers/MockERC20.sol";
-import {MockGateway} from "./helpers/MockGateway.sol";
-import {MockSystemContract} from "./helpers/MockSystemContract.sol";
-
-import {IGatewayZEVM} from "@zetachain/contracts/zevm/interfaces/IGatewayZEVM.sol";
 import {Test, console} from "forge-std/Test.sol";
 
+/**
+ * @dev it's easier to test contracts on testnets
+ */
 contract YieldMilTest is Test {
-    YieldMil yieldMil;
-    MockGateway gateway;
-    MockSystemContract systemContract;
-    MockERC20 usdcBase;
-    MockERC20 ethBase;
-    MockERC20 usdcPolygon;
-    MockERC20 polPolygon;
-
-    function setUp() public {
-        gateway = new MockGateway();
-        systemContract = new MockSystemContract();
-        usdcBase = new MockERC20();
-        ethBase = new MockERC20();
-        usdcPolygon = new MockERC20();
-        polPolygon = new MockERC20();
-        yieldMil = new YieldMil(
-            IGatewayZEVM(address(gateway)),
-            ISystemContract(address(systemContract)),
-            address(usdcBase),
-            address(ethBase),
-            address(usdcPolygon),
-            address(polPolygon)
-        );
-    }
+    function setUp() public {}
 
     function test() public {}
 }
